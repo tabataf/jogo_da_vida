a3117749b68bd9cc0697c219d09bfc71bc8f07ed
tabata Vasques <tabata07defaria@gmail.com>
2022-12-23 19:27:26 -0300

    arrumando quadrado e seus espaços

--- vue/gameoflife/src/components/HelloWorld.vue
+++ vue/gameoflife/src/components/HelloWorld.vue
@@ -1,44 +1,43 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <hr style="margin-top: 0px; color: #d3d6da" />
-    <div class="container-tabuleiro">
-      <div class="tabuleiro">
+    <div class="tabuleiro">
+      <div class="tabuleiro_linha">
         <div id="l1c1" class="quadrado"></div>
         <div id="l1c2" class="quadrado"></div>
         <div id="l1c3" class="quadrado"></div>
         <div id="l1c4" class="quadrado"></div>
         <div id="l1c5" class="quadrado"></div>
       </div>
-      <div class="tabuleiro">
+      <div class="tabuleiro_linha">
         <div id="l2c1" class="quadrado"></div>
         <div id="l2c2" class="quadrado"></div>
         <div id="l2c3" class="quadrado"></div>
         <div id="l2c4" class="quadrado"></div>
         <div id="l2c5" class="quadrado"></div>
       </div>
-      <div class="tabuleiro">
+      <div class="tabuleiro_linha">
         <div id="l3c1" class="quadrado"></div>
         <div id="l3c2" class="quadrado"></div>
         <div id="l3c3" class="quadrado"></div>
         <div id="l3c4" class="quadrado"></div>
         <div id="l3c5" class="quadrado"></div>
       </div>
-      <div class="tabuleiro">
+      <div class="tabuleiro_linha">
         <div id="l4c1" class="quadrado"></div>
         <div id="l4c2" class="quadrado"></div>
         <div id="l4c3" class="quadrado"></div>
         <div id="l4c4" class="quadrado"></div>
         <div id="l4c5" class="quadrado"></div>
       </div>
-      <div class="tabuleiro">
+      <div class="tabuleiro_linha">
         <div id="l5c1" class="quadrado"></div>
         <div id="l5c2" class="quadrado"></div>
         <div id="l5c3" class="quadrado"></div>
         <div id="l5c4" class="quadrado"></div>
         <div id="l5c5" class="quadrado"></div>
       </div>
-      <div class="tabuleiro">
+      <div class="tabuleiro_linha">
         <div id="l6c1" class="quadrado"></div>
         <div id="l6c2" class="quadrado"></div>
         <div id="l6c3" class="quadrado"></div>
@@ -59,18 +58,15 @@ export default {
 </script>
 <style scoped>
 .quadrado {
-  border: 2px solid #d3d6da;
-  width: 38px;
-  height: 38px;
+  border: pink 0.5px solid;
+  border-collapse: separate;
+  width: 20px;
+  height: 20px;
   display: inline-block;
-  margin: 0.7px 0.7px;
 }
-.purple {
-  background-color: rgb(125, 9, 85);
-  border: 1px solid #d3d6da;
-  width: 38px;
-  height: 38px;
-  display: inline-block;
+
+.tabuleiro_linha {
   margin: 0;
+  display: flex;
 }
 </style>
